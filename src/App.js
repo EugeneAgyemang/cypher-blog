0dbac6c7d7d76998273d3e0eae7a63d80c12f458
EugeneAgyemang <eugeneagyemang.ea@gmail.com>
2022-05-10 11:50:38 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+
+import Menu from './Menu'
+import Home from './Home'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create'
+import BlogDetails from './BlogDetails'
+
+
+function App() {
+  
+  return (
+   <Router>
+      <div className="App">
+    <Menu />
+    <div className="content">
+      <Switch>
+        <Route exact path="/">
+        <Home/>
+        </Route>
+        <Route path="/create">
+        <Create/>
+        </Route>
+        <Route path="/blogs/:id">
+        <BlogDetails/>
+        </Route>
+      </Switch>
+    </div> 
+    </div>
+   </Router>
+  );
+}
+
+export default App;
